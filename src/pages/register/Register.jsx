cfe762332ae1962e718585328fe56a31f4c9c429
Aayush1222 <ayushshrestha122@gmail.com>
2024-04-30 15:31:09 +0545

    Adding Validation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,110 +1,157 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-   //Coding Section
+  // Conding Section
 
-   //Make a state variables - 5 STATES
-   
-   //Format (variableName, ChangingVarName) = state
-   
-   const [firstName, setFirstname] = useState('')
-   const [lastName, setLastname] = useState('')
-   const [email, setEmail] = useState('')
-   const [password, setPassword] = useState('')
-   const [confirmPassword, setConfirmPassword] = useState('')
+  //Make a state variables - 5 state
+  // Format (variableName, Changing VarName) = State
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-   //state for Error
-   const [firstNameError, setfirstNameError] = useState('')
-   const [lastNameError, setlastNameError] = useState('')
-   const [emailError, setemailError] = useState('')
-   const [passwordError, setpasswordError] = useState('')
-   const [confirmPasswordError, setconfirmPasswordError] = useState('')
+  //Statw for error
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
+  // Make a function to save the state
+  const handleFirstname = (e) => {
+    setFirstName(e.target.value);
+  };
 
-   //Make a function to save the state
-   const handleFirstname = (e) => {
-    setFirstname(e.target.value)
+  const handleLastname = (e) => {
+    setLastName(e.target.value);
+  };
 
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-   }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-   const handleLastname = (e) =>{
-    setLastname(e.target.value)
-   }
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  };
 
-   const handleEmail = (e) =>{
-    setEmail(e.target.value)
-   }
-
-   const handlePassword = (e) =>{
-    setPassword(e.target.value)
-   }
-
-   const handleconfirmpassword = (e) =>{
-    setConfirmPassword(e.target.value)
-   }
-
-   //Validation
-   const validate = () => {
+  //Validation
+  var validate = () => {
     var isValid = true;
-    if(firstName.trim() == null) {
-        setfirstNameError('Please enter firstname')
-        isValid = false;
+    if (firstName.trim() === "") {
+      setFirstNameError("Please enter firstname");
+      isValid = false;
+    }
+    if (lastName.trim() === "") {
+      setLastNameError("Please enter lastname");
+      isValid = false;
+    }
+    if (email.trim() === "") {
+      setEmailError("Please enter email");
+      isValid = false;
+    }
+    if (password.trim() === "") {
+      setPasswordError("Please enter password");
+      isValid = false;
+    }
+    if (confirmPassword.trim() === "") {
+      setConfirmPasswordError("Please enter Confirm Password");
+      isValid = false;
+    }
+    if (password.trim() !== confirmPassword.trim()) {
+      setConfirmPasswordError("password did not match");
+      isValid = false;
     }
     return isValid;
-   }
+  };
 
-//for button
-   const handleSubmit = (e) => {
-    e.preventDefault()
-    var isValid = validate()
-    if(!isValid){
-        return;  //stop the process
+  //For button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var isValid = validate();
+    if (!isValid) {
+      return; //Stop the process
     }
-    console.log(firstName,lastName,email,password,confirmPassword)
+    console.log(firstName, lastName, email, password, confirmPassword);
+  };
 
-   }
+  return (
+    // <div>
+    //   <h1>Its Register</h1>
+    // </div>
+    <>
+      <div className="m-3">
+        <h1>Create an account</h1>
 
+        <form className="w-50">
+          <label>Firstname:{firstName}</label>
+          <input
+            onChange={handleFirstname}
+            type="text"
+            className="form-control"
+            placeholder="Enter your fistname"
+          />
+          {firstNameError && (
+            <p className="text-danger">{firstNameError}</p> //<small>
+          )}
 
-    return(
-       
-        <>
-        <div className="m-3">
-            <h1> Create an Account!</h1>
-            
-            <form className="w-50">
-                <label>Firstname : {firstName}</label>
-                <input onChange={handleFirstname}type="text" className="form-control" placeholder="Enter your firstname"/>
+          <label className="mt-2">Lastname:{lastName}</label>
+          <input
+            onChange={handleLastname}
+            type="text"
+            className="form-control"
+            placeholder="Enter your lastname"
+          />
+          {lastNameError && (
+            <p className="text-danger">{lastNameError}</p> //<small>
+          )}
+          <label className="mt-2">Email Address: {email}</label>
+          <input
+            onChange={handleEmail}
+            type="email"
+            className="form-control"
+            placeholder="Enter your Email Address"
+          />
+          {emailError && (
+            <p className="text-danger">{emailError}</p> //<small>
+          )}
+          <label className="mt-2">Password:{password}</label>
+          <input
+            onChange={handlePassword}
+            type="text"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+          {passwordError && (
+            <p className="text-danger">{passwordError}</p> //<small>
+          )}
+          <label className="mt-2">Confirm Password: {confirmPassword}</label>
+          <input
+            onChange={handleConfirmPassword}
+            type="text"
+            className="form-control"
+            placeholder="Enter your confirm password"
+          />
+          {confirmPasswordError && (
+            <p className="text-danger">{confirmPasswordError}</p> //<small>
+          )}
+          <button onClick={handleSubmit} className="btn btn-dark mt-2 w-100">
+            Create Account
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-                {
-                    firstNameError && <small>{firstNameError}</small>
-                }
-
-                <label className="mt-2">Lastname : {lastName}</label>
-                <input onChange={handleLastname}type="text" className="form-control" placeholder="Enter your Lastname"/>
-
-                <label className="mt-2">Email Address : {email}</label>
-                <input onChange={handleEmail}type="text" className="form-control" placeholder="Enter your Email"/>
-
-                <label className="mt-2">Password : {password}</label>
-                <input onChange={handlePassword}type="text" className="form-control" placeholder="Enter your Password"/>
-
-                <label className="mt-2">Confirm Password : {confirmPassword}</label>
-                <input onChange={handleconfirmpassword}type="text" className="form-control" placeholder="Enter your Password again"/>
-
-                <button onClick={handleSubmit} className="btn btn-dark mt-2 w-100">Create Acccount</button>
-                
-
-                 
-
-            </form>
-        </div>
-        </>
-
-    );
-}
-
-//exporting
 export default Register;
 
+//Make UI complete
+// Make a Variable to save value if each input temporarily
+// Change everytime we write something
+// submit
