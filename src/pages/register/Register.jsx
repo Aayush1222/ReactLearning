bcd0ad31ae7bed1238e337f76956afac9f263555
Aayush1222 <ayushshrestha122@gmail.com>
2024-04-30 15:03:30 +0545

    Adding updates

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -7,39 +7,100 @@ const Register = () => {
    
    //Format (variableName, ChangingVarName) = state
    
-   const [firstname, setFirstname] = useState('')
-   const [lastname, setLastname] = useState('')
+   const [firstName, setFirstname] = useState('')
+   const [lastName, setLastname] = useState('')
    const [email, setEmail] = useState('')
    const [password, setPassword] = useState('')
    const [confirmPassword, setConfirmPassword] = useState('')
 
+
+   //state for Error
+   const [firstNameError, setfirstNameError] = useState('')
+   const [lastNameError, setlastNameError] = useState('')
+   const [emailError, setemailError] = useState('')
+   const [passwordError, setpasswordError] = useState('')
+   const [confirmPasswordError, setconfirmPasswordError] = useState('')
+
+
+   //Make a function to save the state
+   const handleFirstname = (e) => {
+    setFirstname(e.target.value)
+
+
+   }
+
+   const handleLastname = (e) =>{
+    setLastname(e.target.value)
+   }
+
+   const handleEmail = (e) =>{
+    setEmail(e.target.value)
+   }
+
+   const handlePassword = (e) =>{
+    setPassword(e.target.value)
+   }
+
+   const handleconfirmpassword = (e) =>{
+    setConfirmPassword(e.target.value)
+   }
+
+   //Validation
+   const validate = () => {
+    var isValid = true;
+    if(firstName.trim() == null) {
+        setfirstNameError('Please enter firstname')
+        isValid = false;
+    }
+    return isValid;
+   }
+
+//for button
+   const handleSubmit = (e) => {
+    e.preventDefault()
+    var isValid = validate()
+    if(!isValid){
+        return;  //stop the process
+    }
+    console.log(firstName,lastName,email,password,confirmPassword)
+
+   }
+
+
     return(
+       
+        <>
         <div className="m-3">
             <h1> Create an Account!</h1>
             
             <form className="w-50">
-                <label>Firstname</label>
-                <input type="text" className="form-control" placeholder="Enter your firstname"/>
+                <label>Firstname : {firstName}</label>
+                <input onChange={handleFirstname}type="text" className="form-control" placeholder="Enter your firstname"/>
 
-                <label className="mt-2">Lastname</label>
-                <input type="text" className="form-control" placeholder="Enter your Lastname"/>
+                {
+                    firstNameError && <small>{firstNameError}</small>
+                }
 
-                <label className="mt-2">Email Address</label>
-                <input type="text" className="form-control" placeholder="Enter your Email"/>
+                <label className="mt-2">Lastname : {lastName}</label>
+                <input onChange={handleLastname}type="text" className="form-control" placeholder="Enter your Lastname"/>
 
-                <label className="mt-2">Password</label>
-                <input type="text" className="form-control" placeholder="Enter your Password"/>
+                <label className="mt-2">Email Address : {email}</label>
+                <input onChange={handleEmail}type="text" className="form-control" placeholder="Enter your Email"/>
 
-                <label className="mt-2">Confirm Password</label>
-                <input type="text" className="form-control" placeholder="Enter your Password again"/>
+                <label className="mt-2">Password : {password}</label>
+                <input onChange={handlePassword}type="text" className="form-control" placeholder="Enter your Password"/>
 
-                <button className="btn btn-dark mt-2 w-100">Create Acccount</button>
+                <label className="mt-2">Confirm Password : {confirmPassword}</label>
+                <input onChange={handleconfirmpassword}type="text" className="form-control" placeholder="Enter your Password again"/>
+
+                <button onClick={handleSubmit} className="btn btn-dark mt-2 w-100">Create Acccount</button>
                 
 
                  
 
             </form>
         </div>
+        </>
 
     );
 }
