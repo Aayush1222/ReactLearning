eb54712e2c71658cd33d8010198c18aee7311b65
Aayush1222 <ayushshrestha122@gmail.com>
2024-05-14 09:46:17 +0545

    adding login validations

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,29 +2,72 @@ import React,{useState} from "react";
 
 const Login = () => {
 //use state
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+//error state
+const [emailError, setEmailError] = useState('')
+const [passwordError, setPasswordError] = useState('')
+
+
+//validation function
+var validation = () => {
+    let isValid = true;
+
+    if (email === '' || email.includes('@' === false)) {
+        setEmailError('Email is empty or invalid')
+        isValid = false
+    }
+
+    if (password.trim() === ''){
+        setPasswordError('Password is Empty')
+        isValid = false
+    }
+
+    //return the value
+    return isValid;
+}
+
+// login funtion
+const handleLogin = (e) => {
+    e.preventDefault()
+
+    //validation
+    if(!validation()) {
+        return;
+    }
+
+    console.log(email,password)
+}
 
 
 
 
-    return(
-        <div className="container">
-            <h1> Login to your Account!</h1>
+return(
+    <div className="container">
+        <h1> Login to your Account!</h1>
 
-            <form>
-                <label>Enter your email : {email}</label>
-                <input onChange = {(e)=>setEmail(e.target.value)} type='email' className="form-control" placeholder="Enter your email"/>
+        <form>
+            <label>Enter your email : {email}</label>
+            <input onChange = {(e)=>setEmail(e.target.value)} type='email' className="form-control" placeholder="Enter your email"/>
 
-                <label>Enter your Password</label>
-                <input onChange = {(e)=>setPassword(e.target.value)} type='Password' className="form-control" placeholder="Enter your password"/>
+            {
+                emailError && <p className="text-danger">{emailError}</p>
+            }
 
-                <button className="btn btn-danger w-100 mt-2">Login</button>
-            </form>
-        </div>
+            <label>Enter your Password</label>
+            <input onChange = {(e)=>setPassword(e.target.value)} type='Password' className="form-control" placeholder="Enter your password"/>
+            
+            {
+                passwordError && <p className="text-danger">{passwordError}</p>
+            }
+
+            <button onClick={handleLogin} className="btn btn-danger w-100 mt-2">Login</button>
+        </form>
+    </div>
 
 
-    );
+);
 }
 
 //exporting
